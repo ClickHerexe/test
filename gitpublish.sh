d676101ed608a9530a3f9c6950714ea3860d7481
clickherexe <clickherexe@gmail.com>
2025-05-14 13:41:00 +0200

    test

--- gitpublish.sh
+++ gitpublish.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+git init
 git add .
+git add README.md
 read -p "Descripción del commit: " desc
 git commit -m "$desc"
-git branch -M master
+git branch -M main
 read -p "Nombre del repositorio: " repo
 git remote add origin git@github.com:ClickHerexe/"$repo".git
-git push -u origin master
+git push -u origin main
